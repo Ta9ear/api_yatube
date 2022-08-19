b5db799f44fd0a8cdf77d4ae6916975fd3c8d954
Ta9ear <Ta.9ear@gmail.com>
2022-08-19 11:38:11 +0300

    Third

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class AuthorOrReadOnly(permissions.BasePermission):
+class AuthenticatedAuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_authenticated
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
 from posts.models import Group, Post
-from .permissions import AuthorOrReadOnly
+from .permissions import AuthenticatedAuthorOrReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (AuthenticatedAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -22,15 +22,13 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (AuthenticatedAuthorOrReadOnly,)
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
 
     def perform_create(self, serializer):
-        post_id = self.kwargs.get('post_id')
-        post = get_object_or_404(Post, pk=post_id)
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user, post=self.get_post())
 
     def get_queryset(self):
-        post_id = self.kwargs.get('post_id')
-        post = get_object_or_404(Post, pk=post_id)
-        new_queryset = post.comments
-        return new_queryset
+        return self.get_post().comments
